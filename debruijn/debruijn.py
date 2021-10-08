1b0a0da741fe767ddaedeb06b2c38798aaebe278
Laura Xénard <laura.xenard@protonmail.com>
2021-10-08 12:31:47 +0200

    read_fastq implementation and add fastq files

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -13,25 +13,31 @@
 
 """Perform assembly based on debruijn graph."""
 
-import argparse
-import os
-import sys
-#import networkx as nx
-import matplotlib
 from operator import itemgetter
-import random
-random.seed(9001)
-from random import randint
+import matplotlib.pyplot as plt
+import pickle
+import networkx as nx
+import sys
 import statistics
+import os
+import argparse
+import random
+from random import randint
+random.seed(9001)
 
-__author__ = "Your Name"
-__copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+
+__author__ = "Laura Xénard"
+__copyright__ = "Universite de Paris"
+__credits__ = ["Laura Xénard"]
 __license__ = "GPL"
 __version__ = "1.0.0"
-__maintainer__ = "Your Name"
-__email__ = "your@email.fr"
+__maintainer__ = "Laura Xénard"
+__email__ = "laura.xenard@protonmail.com"
 __status__ = "Developpement"
+
+
+parse = False
+
 
 def isfile(path):
     """Check if path is an existing file.
@@ -52,8 +58,7 @@ def get_arguments():
       Returns: An object that contains the arguments
     """
     # Parsing arguments
-    parser = argparse.ArgumentParser(description=__doc__, usage=
-                                     "{0} -h"
+    parser = argparse.ArgumentParser(description=__doc__, usage="{0} -h"
                                      .format(sys.argv[0]))
     parser.add_argument('-i', dest='fastq_file', type=isfile,
                         required=True, help="Fastq file")
@@ -68,7 +73,28 @@ def get_arguments():
 
 
 def read_fastq(fastq_file):
-    pass
+
+    with open(fastq_file, 'r') as f:
+        for line in f:
+            # We want to get 1 line out of 4.
+            line = next(f)
+            yield line.strip()
+            # We need to strip to remove the trailing \n.
+            line = next(f)
+            line = next(f)
+
+# =============================================================================
+#         line = f.readline()
+#         while line != '':
+#             print(line)
+#             line = f.readline()
+# =============================================================================
+
+
+# =============================================================================
+#             yield f.next()
+#             f.next()
+# =============================================================================
 
 
 def cut_kmer(read, kmer_size):
@@ -86,37 +112,47 @@ def build_graph(kmer_dict):
 def remove_paths(graph, path_list, delete_entry_node, delete_sink_node):
     pass
 
+
 def std(data):
     pass
 
 
-def select_best_path(graph, path_list, path_length, weight_avg_list, 
+def select_best_path(graph, path_list, path_length, weight_avg_list,
                      delete_entry_node=False, delete_sink_node=False):
     pass
+
 
 def path_average_weight(graph, path):
     pass
 
+
 def solve_bubble(graph, ancestor_node, descendant_node):
     pass
+
 
 def simplify_bubbles(graph):
     pass
 
+
 def solve_entry_tips(graph, starting_nodes):
     pass
+
 
 def solve_out_tips(graph, ending_nodes):
     pass
 
+
 def get_starting_nodes(graph):
     pass
+
 
 def get_sink_nodes(graph):
     pass
 
+
 def get_contigs(graph, starting_nodes, ending_nodes):
     pass
+
 
 def save_contigs(contigs_list, output_file):
     pass
@@ -126,20 +162,23 @@ def fill(text, width=80):
     """Split text with a line return to respect fasta format"""
     return os.linesep.join(text[i:i+width] for i in range(0, len(text), width))
 
+
 def draw_graph(graph, graphimg_file):
     """Draw the graph
-    """                                    
+    """
     fig, ax = plt.subplots()
-    elarge = [(u, v) for (u, v, d) in graph.edges(data=True) if d['weight'] > 3]
-    #print(elarge)
-    esmall = [(u, v) for (u, v, d) in graph.edges(data=True) if d['weight'] <= 3]
-    #print(elarge)
+    elarge = [(u, v)
+              for (u, v, d) in graph.edges(data=True) if d['weight'] > 3]
+    # print(elarge)
+    esmall = [(u, v)
+              for (u, v, d) in graph.edges(data=True) if d['weight'] <= 3]
+    # print(elarge)
     # Draw the graph with networkx
-    #pos=nx.spring_layout(graph)
+    # pos=nx.spring_layout(graph)
     pos = nx.random_layout(graph)
     nx.draw_networkx_nodes(graph, pos, node_size=6)
     nx.draw_networkx_edges(graph, pos, edgelist=elarge, width=6)
-    nx.draw_networkx_edges(graph, pos, edgelist=esmall, width=6, alpha=0.5, 
+    nx.draw_networkx_edges(graph, pos, edgelist=esmall, width=6, alpha=0.5,
                            edge_color='b', style='dashed')
     #nx.draw_networkx(graph, pos, node_size=10, with_labels=False)
     # save image
@@ -150,21 +189,35 @@ def save_graph(graph, graph_file):
     """Save the graph with pickle
     """
     with open(graph_file, "wt") as save:
-            pickle.dump(graph, save)
+        pickle.dump(graph, save)
 
 
-#==============================================================
+# ==============================================================
 # Main program
-#==============================================================
+# ==============================================================
 def main():
     """
     Main program function
     """
     # Get arguments
-    args = get_arguments()
+    if parse:
+        args = get_arguments()
+        input_file = args.fastq_file
+        kmer_size = args.kmer_size
+        output_file = args.output_file
+        graph_file = args.graphimg_file
+    else:
+        input_file = '/home/sdv/m2bi/lxenard/Documents/Omiques/Assemblage_metagenomique/debruijn-tp/data/eva71_two_reads.fq'
+        kmer_size = 5
+        output_file = ''
+        graph_file = ''
 
+    seq_gen = read_fastq(input_file)
+    for seq in seq_gen:
+        print(type(seq))
+        print(seq)
     # Fonctions de dessin du graphe
-    # A decommenter si vous souhaitez visualiser un petit 
+    # A decommenter si vous souhaitez visualiser un petit
     # graphe
     # Plot the graph
     # if args.graphimg_file:
